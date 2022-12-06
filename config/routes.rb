e9e1325b2eec0ce6ee0aba0a6e5dff0151d2c3f3
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-12-06 08:40:10 +0300

    configured routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   
-  resources :reviews
-  resources :visits, only: [:index, :show, :create]
-  resources :houses, only: [:index, :show]
-  resources :users, only: [:index, :show, :create]
+  # resources :reviews
+  # resources :visits, only: [:index, :show, :create]
+  # resources :houses, only: [:index, :show]
+  # resources :users, only: [:index, :show, :create]
 
-  get '/authorized_user', to: 'users#show'
+  # get '/authorized_user', to: 'users#show'
 
-  get '/visits/by_user/:id', to: 'visits#user_visits'
+  # get '/visits/by_user/:id', to: 'visits#user_visits'
   
-  get '/reviews/by_user/:id', to: 'reviews#user_reviews' 
+  # get '/reviews/by_user/:id', to: 'reviews#user_reviews' 
 
 
-  post '/login', to: 'sessions#login'
-  delete '/logout', to: 'sessions#logout'
+  # post '/login', to: 'sessions#login'
+  # delete '/logout', to: 'sessions#logout'
 
   
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # # Routing logic: fallback requests for React Router.
+  # # Leave this here to help deploy your app later!
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
