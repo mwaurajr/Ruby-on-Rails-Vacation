353f9b7680c4fe8106e2a9122b2e971162af534d
Frankiline <frankiline.mwaura@student.moringaschool.com>
2022-12-06 08:47:32 +0300

    provide a logic to enable smooth deployment

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#logout'
 
   
-  # # Routing logic: fallback requests for React Router.
-  # # Leave this here to help deploy your app later!
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
